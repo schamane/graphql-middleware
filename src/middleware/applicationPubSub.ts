dd16541ed26bedb5de418bff257a3ae758bfa49a
Nazar Kulyk <schamane@myeburg.net>
2021-09-10 22:04:47 +0200

    Update interfaces for redis pubsub

--- src/middleware/applicationPubSub.ts
+++ src/middleware/applicationPubSub.ts
@@ -1,25 +1,20 @@
 import { RedisPubSub } from 'graphql-redis-subscriptions';
 import { PubSub } from 'graphql-subscriptions';
 import Redis from 'ioredis';
+import type { RedisOptions } from 'ioredis';
 
-export interface PubSubRedisOptions {
-  port?: number;
-  host?: string;
-  options?: Redis.RedisOptions;
-}
+const localPubSub = (options: Record<string, unknown> | undefined) => new PubSub(options);
 
-const localPubSub = () => new PubSub();
-
-const redisPubSub = (options: Record<string, unknown> | undefined, redisOptions: PubSubRedisOptions | undefined) =>
+const redisPubSub = (options: Record<string, unknown> | undefined, redisOptions: RedisOptions | undefined) =>
   new RedisPubSub({
     ...options,
     publisher: new Redis(redisOptions),
     subscriber: new Redis(redisOptions)
   });
 
-export const createPubSub = (options: Record<string, unknown> | undefined, redisOptions?: PubSubRedisOptions): PubSub | RedisPubSub => {
-  if (options) {
+export const createPubSub = (options: Record<string, unknown> | undefined = undefined, redisOptions?: RedisOptions): PubSub | RedisPubSub => {
+  if (redisOptions) {
     return redisPubSub(options, redisOptions);
   }
-  return localPubSub();
+  return localPubSub(options);
 };
